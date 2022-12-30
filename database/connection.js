7773106ce75532af0f50db1b7c7e5c3e97f255c2
= <=>
2022-12-30 22:28:46 +0530

    modified

--- database/connection.js
+++ database/connection.js
@@ -3,38 +3,25 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-let connection = null 
-
-mysql.createConnection({
+const pool = mysql.createPool({
+    connectionLimit: 2,
+    namedPlaceholders: true,
     host: process.env.DB_HOST, 
     user: process.env.DB_USER, 
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD
 })
-.then(db => {
-    connection = db 
-    connection.config.namedPlaceholders = true
-})
-.catch(error => {
-    console.log(error);
-})
+
+
 
 export const fetch = async(sql, params) => {
-    await connection.connect()
-
-    const [result] = await connection.execute(sql, params);
-
-    await connection.end()
+    const [result] = await pool.execute(sql, params);
 
     return result?.[0]
 }
 
 export const query = async(sql, params) => {
-    await connection.connect()
-
-    const [result] = await connection.execute(sql, params)
-
-    await connection.end()
+    const [result] = await pool.execute(sql, params)
 
     return result
 }
