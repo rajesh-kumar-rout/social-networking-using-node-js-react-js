12bd1bc1ca9e32aa506c7dc94da1ab28901e3b87
= <=>
2022-12-30 21:51:14 +0530

    modified

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const app = express()
 
 app.use(express.static("views"))
 
-app.set('view engine', 'ejs');
-// a
+app.set('view engine', 'ejs')
+
 app.use(cors())
-app.use(fileUpload({ useTempFiles: true }))
+app.use(fileUpload({ useTempFiles: true, tempFileDir : "/temp/" }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(setUpRequest)
