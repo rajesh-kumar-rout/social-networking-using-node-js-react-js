1347b0d27b0e45114faf522495f3a83132a92f96
= <=>
2023-01-14 13:09:07 +0530

    8th commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { config } from "dotenv"
 import express from "express"
-import { authenticate } from "./middlewares/authentication.js"
+import { authenticate, isAuthenticated } from "./middlewares/authentication.js"
 import authRoutes from "./routes/auth.js"
 import postRoutes from "./routes/posts.js"
 import userRoutes from "./routes/users.js"
@@ -15,9 +15,9 @@ app.use(express.json({ limit: "1mb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(authenticate)
 
-app.use("/api/users", authenticate, userRoutes)
+app.use("/api/users", isAuthenticated, userRoutes)
 app.use("/api/auth", authRoutes)
-app.use("/api/posts", authenticate, postRoutes)
+app.use("/api/posts", isAuthenticated, postRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening to port ${process.env.PORT}`)
