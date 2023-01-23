2c4bf62bd06b0cc2e0fd79e55bde8bcd2d170cb0
= <=>
2023-01-23 17:53:01 +0530

    db config changed

--- utils/database.js
+++ utils/database.js
@@ -10,6 +10,11 @@ export default knex({
         port: 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    }
+        database: process.env.DB_NAME,
+    },
+    pool: {
+        min: 1,
+        max: 1
+    },
+    acquireConnectionTimeout: 90000
 })
