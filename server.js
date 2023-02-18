6eec466c7524559321add52b1cbece312d6b6d6d
= <=>
2023-02-18 22:24:21 +0530

    modified

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ mongoose.connect(process.env.MONGO_URL);
 
 const app = express()
 
+app.use(express.static("public"))
 app.use(cors())
 app.use(express.json({ limit: "1mb" }))
 app.use(express.urlencoded({ extended: true }))
@@ -21,6 +22,10 @@ app.use(authenticate)
 app.use("/api/users", isAuthenticated, userRoutes)
 app.use("/api/auth", authRoutes)
 app.use("/api/posts", isAuthenticated, postRoutes)
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve("public/index.html"))
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening to port ${process.env.PORT}`)
